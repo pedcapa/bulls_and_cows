d0cf03764a61b3bc64384dd0dffe76c764585b7c
Pedro Castañeda <catbox.py@gmail.com>
2025-03-26 21:53:15 -0600

    Got code working with test: Scores a guess with one Bull

--- lib/bulls_and_cows.ex
+++ lib/bulls_and_cows.ex
@@ -1,5 +1,12 @@
 defmodule BullsAndCows do
-  def score_guess(_secret, _guess) do
-    "0 Bulls, 0 Cows"
+  def score_guess(secret, guess) do
+    secret_list = String.graphemes(secret)
+    guess_list  = String.graphemes(guess)
+
+    bulls =
+      Enum.zip(secret_list, guess_list)
+      |> Enum.count(fn {s, g} -> s == g end)
+
+    "#{bulls} Bulls, 0 Cows"
   end
 end
--- test/bulls_and_cows_test.exs
+++ test/bulls_and_cows_test.exs
@@ -5,4 +5,14 @@ defmodule BullsAndCowsTest do
     assert BullsAndCows.score_guess("1234", "5678") == "0 Bulls, 0 Cows"
     assert BullsAndCows.score_guess("2345", "6789") == "0 Bulls, 0 Cows"
   end
+
+  test "Scores a guess with one Bull" do
+    assert BullsAndCows.score_guess("1234", "1678") == "1 Bulls, 0 Cows"
+    assert BullsAndCows.score_guess("2345", "6785") == "1 Bulls, 0 Cows"
+  end
+
+  test "Scores a guess with two Bulls" do
+    assert BullsAndCows.score_guess("1234", "5236") == "2 Bulls, 0 Cows"
+    assert BullsAndCows.score_guess("2345", "2685") == "2 Bulls, 0 Cows"
+  end
 end
